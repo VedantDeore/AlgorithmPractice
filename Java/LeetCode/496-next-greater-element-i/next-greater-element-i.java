8cb4e114ba516abfa10dffdb29a49f1748147adf
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-07-13 13:26:43 +0530

    Time: 3 ms (92.58%) | Memory: 44.6 MB (11.91%) - LeetSync

--- Java/LeetCode/496-next-greater-element-i/next-greater-element-i.java
+++ Java/LeetCode/496-next-greater-element-i/next-greater-element-i.java
@@ -2,19 +2,20 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> ngeMap = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            int num = nums2[i];
-            while (!stack.isEmpty() && stack.peek() <= num) {
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
                 stack.pop();
             }
-            int nextGreater = stack.isEmpty() ? -1 : stack.peek();
-            ngeMap.put(num, nextGreater);
-            stack.push(num);
+            if(stack.isEmpty()){
+                ngeMap.put(nums2[i],-1);
+            }else{
+                ngeMap.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
         }
-
-        int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = ngeMap.get(nums1[i]);
+        int ans[] = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+           ans[i] = ngeMap.get(nums1[i]);
         }
         return ans;
     }
