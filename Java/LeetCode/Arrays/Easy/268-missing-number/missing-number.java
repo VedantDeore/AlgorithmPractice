55d4a0d732e11190f1df6e7449314e0768405b79
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-07-18 11:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (31.07%) - LeetSync

--- Java/LeetCode/Arrays/Easy/268-missing-number/missing-number.java
+++ Java/LeetCode/Arrays/Easy/268-missing-number/missing-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n= nums.length;
-        int sum = n*(n+1)/2;
+        int sum = n*(n+1)>>1;
         int ans=0;
         for(int i=0;i<n;i++){
             ans+=nums[i];
