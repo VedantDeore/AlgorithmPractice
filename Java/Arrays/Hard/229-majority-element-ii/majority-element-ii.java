36f0ef3c5b2746c7394d11896211474cbd035405
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-07-25 17:19:29 +0530

    Time: 2 ms (99.84%) | Memory: 50.6 MB (27.29%) - LeetSync

--- Java/Arrays/Hard/229-majority-element-ii/majority-element-ii.java
+++ Java/Arrays/Hard/229-majority-element-ii/majority-element-ii.java
@@ -1,15 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-       Map<Integer, Integer> map = new HashMap<>();
-       for(Integer num:nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-       }
-       List<Integer> ans = new ArrayList<>();
-       for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-        if(entry.getValue()> (nums.length/3)){
-            ans.add(entry.getKey());
+      List<Integer> ans  = new ArrayList<>();
+      int cnt1=0,cnt2=0;
+      int el1 = Integer.MIN_VALUE,el2 = Integer.MAX_VALUE;
+      for(int i=0;i<nums.length;i++){
+        if(cnt1==0 && nums[i] != el2){
+            cnt1++;
+            el1 = nums[i];
+        }else if(cnt2 == 0 && nums[i] != el1){
+            cnt2++;
+            el2 = nums[i];
+        }else if(nums[i] == el1){
+            cnt1++;
+        }else if(nums[i] == el2){
+            cnt2++;
+        }else{
+            cnt1--;
+            cnt2--;
         }
-       }
-       return ans;
+      }
+      cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
+        }
+      int min = (int)(nums.length / 3) + 1;
+        if (cnt1 >= min) ans.add(el1);
+        if (cnt2 >= min) ans.add(el2);
+      return ans;
     }
 }
