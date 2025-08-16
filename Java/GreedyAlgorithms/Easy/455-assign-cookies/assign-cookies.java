85d7bd471955b3da6375f701fafb49a0b1ba3cec
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-08-16 11:58:36 +0530

    Time: 10 ms (78.89%) | Memory: 48.6 MB (45.46%) - LeetSync

---
+++ Java/GreedyAlgorithms/Easy/455-assign-cookies/assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] greed, int[] cookieSize) {
+        int n = greed.length;
+        int m = cookieSize.length;
+
+        Arrays.sort(greed);
+        Arrays.sort(cookieSize);
+        int l = 0;      
+        int r = 0;
+        while (l < m && r < n) {           
+            if (greed[r] <= cookieSize[l]) {                
+                r++;
+            }            
+            l++;
+        }
+        return r;
+    }
+}
