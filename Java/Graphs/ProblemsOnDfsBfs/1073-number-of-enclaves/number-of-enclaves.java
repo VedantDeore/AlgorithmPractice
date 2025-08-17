52a6fa5e24157cdc964fc528214993cb4a53b15d
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-08-17 23:15:35 +0530

    Time: 11 ms (16.43%) | Memory: 54.6 MB (75.85%) - LeetSync

--- Java/Graphs/ProblemsOnDfsBfs/1073-number-of-enclaves/number-of-enclaves.java
+++ Java/Graphs/ProblemsOnDfsBfs/1073-number-of-enclaves/number-of-enclaves.java
@@ -1,16 +1,12 @@
-class Solution {
-    private void dfs(int row,int col,int grid[][],boolean visited[][], int delRow[],int delCol[]){
-        visited[row][col] = true;
-
-        for(int i=0;i<4;i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-
-            if(nrow >=0 && nrow < grid.length && ncol >= 0 && ncol < grid[0].length && !visited[nrow][ncol] && grid[nrow][ncol] == 1 ){
-                dfs(nrow,ncol,grid,visited,delRow,delCol);
-            }
-        }
+class Pair{
+    int first;
+    int second;
+    Pair(int first, int second){
+        this.first = first;
+        this.second = second;
     }
+}
+class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -19,27 +15,29 @@ class Solution {
 
         int delRow[] = {-1,0,1,0};
         int delCol[] = {0,1,0,-1};
-        // col
-        for(int j = 0;j<n;j++){
-            
-            if(!visited[0][j] && grid[0][j] == 1){
-                dfs(0,j,grid,visited,delRow,delCol);
-            }
 
-            if(!visited[m-1][j] && grid[m-1][j] == 1){
-                dfs(m-1,j,grid,visited,delRow,delCol);
+        Queue<Pair> q = new LinkedList<Pair>();
+        // corner row, col
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1 && (i==0 || j==0 || i== m-1 || j==n-1)){
+                    q.add(new Pair(i,j));
+                    visited[i][j] = true;
+                }
             }
         }
+        while(!q.isEmpty()){
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.remove();
 
-        // row
-        for(int i=0;i<m;i++){
-            
-            if(!visited[i][0] && grid[i][0] == 1){
-                dfs(i,0,grid,visited,delRow,delCol);
-            }
-
-            if(!visited[i][n-1] && grid[i][n-1] == 1){
-                dfs(i,n-1,grid,visited,delRow,delCol);
+            for(int i=0;i<4;i++){
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                if(nrow>=0&& nrow<m && ncol>=0 && ncol<n && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
+                    visited[nrow][ncol] = true;
+                    q.add(new Pair(nrow,ncol));
+                }
             }
         }
 
