208f85ef6c48188f7094b10e04d8e6efed54bc39
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-08-25 16:55:42 +0530

    Time: 3 ms (79.85%) | Memory: 46.2 MB (23.76%) - LeetSync

---
+++ Java/GreedyAlgorithms/Hard/135-candy/candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int N = ratings.length;
+        int left[] = new int[N];
+        left[0] = 1;
+        int right[] = new int[N];
+        right[N-1] = 1;
+        for(int i=1;i<N;i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }else{
+                left[i] = 1;
+            }
+        }
+        for(int i=N-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1] +1;
+            }else{
+                right[i] = 1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum = sum +  Math.max(left[i], right[i]);
+        }
+        return sum;
+    }
+}
