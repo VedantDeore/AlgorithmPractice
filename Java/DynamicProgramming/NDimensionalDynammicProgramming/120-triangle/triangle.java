23c088b549891e4a87529641f1b1fd352d85cd0f
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-08-28 15:18:06 +0530

    Time: 3 ms (70.38%) | Memory: 45.2 MB (62.10%) - LeetSync

--- Java/DynamicProgramming/NDimensionalDynammicProgramming/120-triangle/triangle.java
+++ Java/DynamicProgramming/NDimensionalDynammicProgramming/120-triangle/triangle.java
@@ -2,16 +2,15 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int front[] = new int[n];
-        int idx=0;
-        for(int element: triangle.get(n-1)){
-            front[idx++] = element;
+        for (int j = 0; j < n; j++) {
+            front[j] = triangle.get(n-1).get(j);
         }
         for(int i=n-2;i>=0;i--){
             int curr[] = new int[n];
             for(int j=i; j>=0; j--){
-                int d = triangle.get(i).get(j) + front[j];
-                int dg = triangle.get(i).get(j) + front[j+1];
-                curr[j] = Math.min(d,dg);
+                int d = front[j];
+                int dg = front[j+1];
+                curr[j] = triangle.get(i).get(j) + Math.min(d,dg);
             }
             front = curr;
         }
