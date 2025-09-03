ba755a8dd6f7a5a7f5b4771be5a4fb2836488c85
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-09-03 17:41:05 +0530

    Time: 37 ms (99.42%) | Memory: 49.7 MB (25.40%) - LeetSync

--- Java/AdvancedMaths/204-count-primes/count-primes.java
+++ Java/AdvancedMaths/204-count-primes/count-primes.java
@@ -1,26 +1,23 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n <= 2)return 0;
-        int prime[] = new int[n+1];
+        if(n < 3)return 0;
+        
+        boolean prime[] = new boolean[n+1];
+        // false = prime, true = composite
 
-        Arrays.fill(prime, 1); 
-        prime[0] = 0;
-        prime[1] = 0;
+        int count = n / 2;  
+        // all odds -> prime
 
-        for(int i=2; i*i<=n;i++){
-            if(prime[i] == 1){
-                for(int j= i*i; j<=n; j+=i){
-                    prime[j] = 0;
+        for(int i=3; i*i<n; i+=2){
+            if (prime[i]) continue;
+            for(int j= i*i; j<n; j+=2*i){
+                if (!prime[j]) {
+                    --count;     // found -> composite
+                    prime[j] = true;
                 }
             }
+            
         }
-
-        int cnt=0;
-        for(int i =2 ;i<n;i++){
-            if(prime[i] == 1){
-                cnt++;
-            }
-        }
-        return cnt;
+        return count;
     }
 }
