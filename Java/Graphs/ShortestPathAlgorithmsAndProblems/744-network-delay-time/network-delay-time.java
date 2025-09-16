9c42cce31ecb17de3ac11f7c6b0384e3b752a0a6
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-09-16 14:25:18 +0530

    Time: 9 ms (93.57%) | Memory: 49 MB (39.30%) - LeetSync

--- Java/Graphs/ShortestPathAlgorithmsAndProblems/744-network-delay-time/network-delay-time.java
+++ Java/Graphs/ShortestPathAlgorithmsAndProblems/744-network-delay-time/network-delay-time.java
@@ -1,48 +1,47 @@
 class Solution {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        // src -> [dst,cost]
+    public int networkDelayTime(int [][]times, int n , int k){
         ArrayList<ArrayList<int[]>> adjList = new ArrayList<>();
-        for(int i=0;i<=n;i++){
+        for (int i = 0; i <= n; i++) {
             adjList.add(new ArrayList<>());
         }
-        int m = times.length;
-        for(int i=0;i<m;i++){
+
+        int m=times.length;
+        
+        for (int i = 0; i <m; i++) {
             int u = times[i][0];
             int v = times[i][1];
             int time = times[i][2];
-            adjList.get(u).add(new int[]{v,time});
+            adjList.get(u).add(new int[]{v, time});
         }
 
-        // time, src, dist
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,k,0});
-
         int dist[] = new int[n+1];
-        Arrays.fill(dist,(int)1e9);
-        dist[k] = 0;
-        dist[0] = 0;
-        while(!q.isEmpty()){
+        Arrays.fill(dist, (int)1e9);
+        dist[k] = 0; // src =0;
+        dist[0] = 0; // 1 based indexing 
+        // dist, node
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)-> a[0] - b[0]);
+        q.add(new int[]{0,k});
+        while (!q.isEmpty()) {
             int current[] = q.poll();
             int time = current[0];
             int node = current[1];
-            int distance = current[2];
 
-            for(int[] neighbour : adjList.get(node)){
+            for (int[] neighbour : adjList.get(node)) {
                 int adjNode = neighbour[0];
-                int edgeWeight = neighbour[1]; // travel time
-                if(distance + edgeWeight < dist[adjNode]){
-                    dist[adjNode] = distance+edgeWeight;
-                    q.add(new int[]{time +1, adjNode, distance+edgeWeight});
+                int edgeWeight = neighbour[1];
+                if(time + edgeWeight < dist[adjNode]){
+                    dist[adjNode] = time+ edgeWeight;
+                    q.add(new int[]{time + edgeWeight, adjNode});
                 }
             }
         }
-        int time=0;
-        for(int i=0;i<=n;i++){
+        int mintime =0;
+        for (int i = 1; i <= n; i++) {
             if(dist[i] == (int)1e9)return -1;
             else{
-                time = Math.max(time, dist[i]);
+                mintime = Math.max(mintime, dist[i]);
             }
         }
-        return time;
+        return mintime;
     }
 }
