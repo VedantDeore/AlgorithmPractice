4ad663ec80ac5cd7c3e617da76b1c3ccc9f9117e
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-09-24 16:56:08 +0530

    Time: 4 ms (78.43%) | Memory: 44.8 MB (20.15%) - LeetSync

--- Java/LinkedList/Hard/23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ Java/LinkedList/Hard/23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,34 +9,26 @@
  * }
  */
 class Solution {
-    private ListNode mergerSorted(ListNode head1, ListNode head2){
-        ListNode dummpyNode = new ListNode(-1);
-        ListNode temp = dummpyNode;
-        ListNode temp1 = head1;
-        ListNode temp2 = head2;
-
-        while (temp1 != null && temp2 != null) {
-            if(temp1.val > temp2.val){
-                temp.next = temp2;
-                 temp = temp2;
-                temp2 = temp2.next;
-            }else{
-                temp.next = temp1;
-                temp = temp1;
-                temp1 = temp1.next;
-            }
-        }
-        if(temp1 != null) temp.next = temp1;
-        if(temp2 != null) temp.next = temp2;
-        
-        return dummpyNode.next;
-    }
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists == null || lists.length == 0) return null;
-        ListNode head= lists[0];
-        for(int i=1;i<lists.length;i++){
-            head = mergerSorted(head,lists[i]);
+        PriorityQueue<ListNode> q = new PriorityQueue<>((a,b)-> a.val - b.val);
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                q.add(node);
+            }
         }
-        return head;
+        while (!q.isEmpty()) {
+            ListNode current = q.poll();
+
+            temp.next = current;
+            if(current.next != null){
+                q.add(current.next);
+            }
+            temp = temp.next;
+        }
+        return dummyNode.next;
     }
 }
