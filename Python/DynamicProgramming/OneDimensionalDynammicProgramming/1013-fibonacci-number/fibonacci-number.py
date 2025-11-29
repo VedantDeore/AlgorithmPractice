dd1a17622ca3d71a91c11d70a1ba9d21d063fd56
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-11-29 23:42:04 +0530

    Time: 36 ms (76.88%) | Memory: 17.6 MB (89.06%) - LeetSync

---
+++ Python/DynamicProgramming/OneDimensionalDynammicProgramming/1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if(n==0):
+            return 0
+        if(n==1):
+            return 1
+        prev2=0
+        prev=1
+        for i in range(2,n+1):
+            curr = prev2 + prev
+            prev2 = prev
+            prev= curr
+        return prev
+        
