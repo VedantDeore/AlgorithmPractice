9b192e93505285a08b0305bc1cad3585d39c368c
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2025-11-29 23:34:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (26.18%) - LeetSync

---
+++ Java/DynamicProgramming/OneDimensionalDynammicProgramming/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0){
+            return 0;
+        }else if(n == 1){
+            return 1;
+        }else if(n == 2){
+            return 1;
+        }
+        int prev2=0;
+        int prev1=1;
+        int prev=1;
+        for(int i=3;i<=n;i++){
+            int curr = prev + prev1 + prev2;
+            prev2 = prev1;
+            prev1 = prev;
+            prev = curr; 
+        }
+        return prev;
+    }
+}
