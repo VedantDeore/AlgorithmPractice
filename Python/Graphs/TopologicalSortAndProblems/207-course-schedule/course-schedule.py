89d5dac2545cc80dfe897e97f31feb2fcc6ef74f
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2026-02-14 02:32:05 +0530

    Time: 8 ms (22.77%) | Memory: 20.5 MB (46.22%) - LeetSync

---
+++ Python/Graphs/TopologicalSortAndProblems/207-course-schedule/course-schedule.py
@@ -0,0 +1,44 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList : List[List[int]]  = []
+
+        n = numCourses
+        edges = prerequisites
+        for i in range(n):
+            adjList.append([])
+
+
+        for i in range(len(edges)):
+            u = edges[i][0]
+            v = edges[i][1]
+
+            adjList[u].append(v)
+
+        indegree = [0]*n
+
+        for i in range(n):
+            for neighbour in adjList[i]:
+                indegree[neighbour] +=1
+
+        q = deque()
+
+
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        cnt= 0
+        while q:
+            node = q.popleft()
+            cnt+=1
+
+            for neighbour in adjList[node]:
+                indegree[neighbour] -=1
+                if indegree[neighbour] ==0:
+                    q.append(neighbour)
+
+        if cnt == n:
+            return True
+        return  False
+
+        
