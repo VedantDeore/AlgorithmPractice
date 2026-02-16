4a9c2556ea86dd2eaabbbfcd60cb80e25114ba00
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2026-02-16 16:13:08 +0530

    Time: 45 ms (49.39%) | Memory: 19.2 MB (65.93%) - LeetSync

--- Python/BitManipulation/190-reverse-bits/reverse-bits.py
+++ Python/BitManipulation/190-reverse-bits/reverse-bits.py
@@ -1,18 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        s = []
+        result = 0
+        
         for _ in range(32):
-            s.append(str(n%2))
-            n = n //2
-
-        # print(s)
-        # s= s[::-1]
-        length = len(s)
-        p2=1
-        num=0
-        for i in range(length -1,-1,-1):
-            if(s[i] == '1'):
-                num = num + p2
-            p2 = p2*2
-
-        return num
+            result = (result << 1) | (n & 1)  # take last bit of n
+            n >>= 1                           # shift n right
+        
+        return result
