24a7d772aea4569672976453f4325a9075f8d23e
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2026-03-14 14:41:34 +0530

    Time: 151 ms (96.32%) | Memory: 20.5 MB (96.93%) - LeetSync

--- Python/Daily/March2026/3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.py
+++ Python/Daily/March2026/3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -1,23 +1,32 @@
-from heapq import heappop, heappush
-from typing import List
-
+import math
 class Solution:
+    def check(self, mid:int, workerTimes: List[int], mountainHeight:int)->bool:
+        h = 0
+
+        for t in workerTimes:
+            h += int(math.sqrt((2 * mid / t) + 0.25) - 0.5)
+
+            if h>=mountainHeight:
+                return True
+            
+        return True if h>=mountainHeight else False
+
     def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
-        # Priority queue to store tuples in the form (total_time, (original_time, iteration))
-        pq = []
+        low = 1
+        high = min(workerTimes)*mountainHeight*(mountainHeight+1)//2
 
-        # Initialize the priority queue with worker times
-        for time in workerTimes:
-            heappush(pq, (time, time, 1)) # (current time, original time, iteration count)
+        ans=0
 
-        ans = 0
+        while low<=high:
+            mid = low + ((high - low) >> 1)
 
-        # Process each height increment of the mountain
-        for _ in range(mountainHeight):
-            currentTime, originalTime, iteration = heappop(pq)
-            ans = currentTime
-
-            # Push the next time for this worker back into the priority queue
-            heappush(pq, (currentTime + originalTime * (iteration + 1), originalTime, iteration + 1))
+            if (self.check(mid, workerTimes, mountainHeight)):
+                ans = mid
+                high = mid-1
+            else:
+                low = mid+1
 
         return ans
+                
+
+        
