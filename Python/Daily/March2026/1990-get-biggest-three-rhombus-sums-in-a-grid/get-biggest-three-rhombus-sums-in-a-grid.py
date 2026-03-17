d64f78a8890bbf3558c230ada6b7b6d62aac6052
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2026-03-17 21:18:08 +0530

    Time: 698 ms (53.40%) | Memory: 22.9 MB (26.21%) - LeetSync

--- Python/Daily/March2026/1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.py
+++ Python/Daily/March2026/1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.py
@@ -1,49 +1,35 @@
-diag=[[0]*51 for _ in range(100)]
-antid=[[0]*51 for _ in range(100)]
-OFFSET=50
+import heapq
+
 class Solution:
     def getBiggestThree(self, grid: List[List[int]]) -> List[int]:
-        def rhombusSum(i, j, d):
-            if d==0: return grid[i][j]
-            l, r, u, b=j-d, j+d, i-d, i+d
-            i0, i1=u-j+OFFSET, i-l+OFFSET
-            Sum=diag[i0][r+1]-diag[i0][j]
-            Sum+=diag[i1][j+1]-diag[i1][l]
+        m = len(grid)
+        n = len(grid[0])
 
-            j0, j1=u+j, b+j
-            Sum+=antid[j0][i]-antid[j0][u+1]
-            Sum+=antid[j1][b]-antid[j1][i+1]
-            return Sum
+        top_sums = set()
 
-        m, n=len(grid), len(grid[0])
-        for i, row in enumerate(grid):
-            for j, x in enumerate(row):
-                i0, j0=i-j+OFFSET, i+j
-                diag[i0][j+1]=diag[i0][j]+x
-                antid[j0][i+1]=antid[j0][i]+x
+        def add_val(val):
+            top_sums.add(val)
 
-        dM=min(m, n)>>1
-        x=[-1]*3
-        for d in range(dM+1):
-            for i in range(d, m-d):
-                for j in range(d, n-d):
-                    y=rhombusSum(i, j, d)
-                    if y==x[0] or y==x[1] or y==x[2]: continue
-                    if y>x[0]:
-                        x[2]=x[1]
-                        x[1]=x[0]
-                        x[0]=y
-                    elif y>x[0]:
-                        x[2]=x[1]
-                        x[1]=x[0]
-                        x[0]=y
-                    elif y>x[1]:
-                        x[2]=x[1]
-                        x[1]=y
-                    elif y>x[2]:
-                        x[2]=y
+        def findSum(r, c, s):
+            summ = 0
+            for i in range(s):
+                summ += grid[r - s + i][c + i]     # Top to Right
+                summ += grid[r + i][c + s - i]     # Right to Bottom
+                summ += grid[r + s - i][c - i]     # Bottom to Left
+                summ += grid[r - i][c - s + i]     # Left to Top
+            return summ
 
-        for i in range(2, -1, -1):
-            if x[i]==-1: x.pop()
+        for r in range(m):
+            for c in range(n):
 
-        return x
+                # for 0 side rhombus 
+                add_val(grid[r][c])
+                
+                maxrng = min(m,n)
+                for side in range(1,maxrng):
+                    if (r+side <m and r-side>=0 and c+side<n and c-side>=0):
+                        summ = findSum(r,c,side) 
+                        add_val(summ)
+
+
+        return list(heapq.nlargest(3, top_sums))
