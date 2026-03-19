065621064e423664d1faf9de68b67d347e438162
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2026-03-19 23:05:06 +0530

    Time: 1105 ms (30.11%) | Memory: 156.2 MB (56.99%) - LeetSync

--- Python/Daily/March2026/3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.py
+++ Python/Daily/March2026/3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -2,48 +2,25 @@ class Solution:
     def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-
         ans = 0
-
-        mat = [[0]*m for _ in range(n)]
-        hasX = [[0]*m for _ in range(n)]
-       
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 'X':
-                    mat[i][j] = 1
-                    hasX[i][j] +=1
-                elif grid[i][j] == 'Y':
-                    mat[i][j] = -1
-                else:
-                    mat[i][j] = 0
-
-        for i in range(n):
-            for j in range(1,m):
-                mat[i][j] += mat[i][j-1]
-                hasX[i][j] += hasX[i][j-1]
-        
-        for i in range(1,n):
-            for j in range(m):
-                mat[i][j] += mat[i-1][j]
-                hasX[i][j] += hasX[i-1][j]
-
-
-        
-        # for row in mat:
-        #     print(row)
-            
-        # for row in hasX:
-        #     print(row)
-
+        prefX = [[0] * m for _ in range(n)]
+        prefY = [[0] * m for _ in range(n)]
 
         for i in range(n):
             for j in range(m):
-                if mat[i][j] == 0 and hasX[i][j] >= 1:
-                    ans+=1
+    
+                curX = 1 if grid[i][j] == 'X' else 0
+                curY = 1 if grid[i][j] == 'Y' else 0
+                topX = prefX[i-1][j] if i > 0 else 0
+                leftX = prefX[i][j-1] if j > 0 else 0          
+                topY = prefY[i-1][j] if i > 0 else 0
+                leftY = prefY[i][j-1] if j > 0 else 0             
+                diagX = prefX[i-1][j-1] if (i > 0 and j > 0) else 0
+                diagY = prefY[i-1][j-1] if (i > 0 and j > 0) else 0    
+                prefX[i][j] = curX + topX + leftX - diagX
+                prefY[i][j] = curY + topY + leftY - diagY
+
+                if prefX[i][j] > 0 and prefX[i][j] == prefY[i][j]:
+                    ans += 1
 
         return ans
-
-                    
-            
-                
