69d47ffee8058b3b04c2f55fe0499681e86418b3
Vedant Deore <126149988+VedantDeore@users.noreply.github.com>
2026-04-01 01:54:40 +0530

    Time: 847 ms (22.36%) | Memory: 22.1 MB (87.46%) - LeetSync

--- Python/Daily/March2026/15-3sum/3sum.py
+++ Python/Daily/March2026/15-3sum/3sum.py
@@ -5,6 +5,9 @@ class Solution:
         nums= sorted(nums)
         s : Set[Tuple(int,int,int)]= set()
         for i in range(n):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            
             j=i+1
             k=n-1
             while j<k:
@@ -13,7 +16,9 @@ class Solution:
                     s.add((nums[i], nums[j], nums[k]))
                     j+=1
                     k-=1
-                elif nums[i] + nums[j] + nums[k] <0:
+                    while j<k and nums[j]==nums[j-1]: j+=1
+                    while j<k and nums[k]==nums[k+1]: k-=1
+                elif nums[i] + nums[j] + nums[k] <0:                  
                     j+=1
                 else:
                     k-=1
