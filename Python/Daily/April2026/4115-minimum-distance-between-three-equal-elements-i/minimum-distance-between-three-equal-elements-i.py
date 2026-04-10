2589c9289f3900f17dc7ab010cd19f346a211e07
Vedant Deore <vedantdeore45@gmail.com>
2026-04-10 16:40:10 +0530

    Time: 3 ms (67.11%) | Memory: 19.3 MB (77.52%) - LeetSync

--- Python/Daily/April2026/4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ Python/Daily/April2026/4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -2,11 +2,16 @@ class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         ans= 1000
         n = len(nums)
+        d = defaultdict(list)
         for i in range(n):
-            for j in range(i+1,n):
-                for k in range(j+1,n):
-                    if nums[i] == nums[j] == nums[k]:
-                        ans = min(ans, abs(i-j) + abs(j-k) + abs(k-i))
+            d[nums[i]].append(i)
+
+            if len(d[nums[i]]) >= 3:
+                ans = min(ans, 2*(d[nums[i]][-1] - d[nums[i]][-3]) )
+        
+        # print(d)
+        # return ans if ans!= 1000 else -1
+        
 
         return ans if ans!= 1000 else -1
         
